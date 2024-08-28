ab032660cdd4e8d80787c808315d56e384b105d4
Fatinul Haziq <117275088+fatinul@users.noreply.github.com>
2024-08-28 22:47:14 +1000

    Check if the-mee-null really installed

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Check if running as root
 if [ "${EUID}" -ne 0 ]; then
-    echo -e "This script must be run as root. It's okay,\n > Consider run \"sudo ./install.sh\" instead." >&2
+    echo -e "This script must be run as root. It's okay,\\n > Consider run \\"sudo ./install.sh\\" instead." >&2
     exit 1
 fi
 
@@ -13,5 +13,10 @@ cmake --build build
 # Install the executable to /usr/local/bin
 install -m 755 build/the-mee-null /usr/local/bin/
 
-# echo success
-echo "Installation successful. 'the-mee-null' is now available."
+# Verify installation
+if which the-mee-null &>/dev/null; then
+    echo "Installation successful. 'the-mee-null' is now available."
+else
+    echo "Error: Installation failed. 'the-mee-null' was not installed in /usr/local/bin." >&2
+    exit 1
+fi
