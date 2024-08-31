b503bd637d6c9b842333f813d32a35a53d8bb3f2
fatinul <fatinulhaziq22@gmail.com>
2024-08-31 20:49:28 +1000

    Adding option to remove or keep the-mee-null binary

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,17 @@
 start_line=$(grep -n "THE-MEE-NULL" ~/.bashrc | cut -d: -f1)
 end_line=$(grep -n "END" ~/.bashrc | head -n1 | cut -d: -f1)
 
+echo "Removing section from .bashrc..."
 sed -i "${start_line},${end_line} d" ~/.bashrc
 
-echo "THE-MEE-NULL have been removed from the .bashrc file. Try open new terminal or 'source ~/.bashrc' to see the effect."
+read -p "Remove the-mee-null binary? (y/n): " response
+response=${response,,}
+
+if [ "$response" = "y" ] || [ -z "$response" ]; then
+  echo "Removing the-mee-null binary..."
+  sudo rm /usr/local/bin/the-mee-null
+else
+  echo "Skipping removal of the-mee-null binary."
+fi
+
+echo "Thank you for checking out my program!"
